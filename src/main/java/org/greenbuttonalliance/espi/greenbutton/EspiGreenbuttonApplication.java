4e3794eb618015d27b4d3c1e5d6f6765cb604542
Donald F Coffin <donald.coffin@reminetworks.com>
2019-02-02 00:01:58 -0500

    Initial Project Add

---
+++ src/main/java/org/greenbuttonalliance/espi/greenbutton/EspiGreenbuttonApplication.java
@@ -0,0 +1,14 @@
+package org.greenbuttonalliance.espi.greenbutton;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EspiGreenbuttonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EspiGreenbuttonApplication.class, args);
+    }
+
+}
+
